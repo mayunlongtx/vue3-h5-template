1c80ff5886fc9e0143d14c89d8d436d72dbf9cc5
mayunlongtx <865147643@qq.com>
2023-10-23 13:57:13 +0800

    refactor: 重构路由获取配置逻辑

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue';
-import router from './router/index';
+import routerPromise from './router';
 import './styles/index.scss';
 import 'amfe-flexible';
 import App from './App.vue';
@@ -9,14 +9,18 @@ import Vant from 'vant';
 // 2. 引入组件样式
 import 'vant/lib/index.css';
 import directives from './directive/index';
-import 'uno.css'
+import 'uno.css';
 
 // or init with options
 // const vConsole = new VConsole({ theme: 'dark' });
-const app = createApp(App);
-// directives.install(app);
-app.use(createPinia());
-registerStore();
-app.use(router).use(directives).use(Vant)
 
-app.mount('#app');
+(async () => {
+  const router = await routerPromise;
+  const app = createApp(App);
+  // directives.install(app);
+  app.use(createPinia());
+  registerStore();
+  app.use(router).use(directives).use(Vant);
+
+  app.mount('#app');
+})();
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,37 +1,109 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+// main.ts
+import { createRouter, createWebHistory } from 'vue-router';
+import { MenuTitle } from '@/enums/menu';
+import { composeRight } from '@/utils';
 import { getToken } from '@/utils/index';
-
-import { routes } from './generateRoutes';
 import { LOGIN_OUT_PATH, LOGIN_PATH } from '@/helpers';
-console.log('routes--->', routes);
-const router = createRouter({
-  history: createWebHashHistory(import.meta.env.VITE_PUBLIC_PATH),
-  routes,
-});
-interface routerType {
-  path: string;
-  name: string;
-}
 // 白名单
-const whiteList = [LOGIN_PATH, LOGIN_OUT_PATH, ...routes.map((item: routerType) => item.path)];
-let routerStack: string[] = [];
-router.beforeEach((to: any, from: any, next) => {
-  if (!getToken() && !whiteList.includes(to.path)) {
-    next(LOGIN_OUT_PATH);
-  } else {
-    if (to.name === routerStack[routerStack.length - 1]) {
-      // 这里是后退
-      routerStack.pop();
-      from.meta.keepAlive = false;
-      to.meta.keepAlive = true;
-    } else {
-      // 这里是前进
-      routerStack.push(to.name);
+const whiteList = [LOGIN_PATH, LOGIN_OUT_PATH];
+async function generateRoutes() {
+  const routeFiles = import.meta.glob('../pages/**/*.vue');
+  const routes: any = [];
+
+  for (const path in routeFiles) {
+    const importer = routeFiles[path];
+    const newPath = path.replace('../pages/', './');
+    const modulePath = newPath.replace(/^\.\/(.*)\.\w+$/, '$1');
+    let name = modulePath.replace(/\/index$/, '');
+
+    const firstInd = modulePath.indexOf('/');
+    const lastInd = modulePath.lastIndexOf('/');
+
+    const file: any = await importer();
+    const component = file.default;
+
+    const firstName = name.substring(0, firstInd);
+    const lastName = modulePath.substr(lastInd + 1, modulePath?.length);
+
+    if (lastInd !== -1 && lastName === 'middle_page') {
+      name = name.substr(0, lastInd);
     }
-    document.title = to.meta.title;
-    setTimeout(() => {
-      next();
+
+    let title = component.pageTitle;
+    if (!component.pageTitle) {
+      title = MenuTitle[firstName.toUpperCase()] || MenuTitle.DEFAULT;
+    }
+
+    routes.push({
+      path: `/${name === 'index' ? '' : name.toLowerCase()}`,
+      name: conversionName(name),
+      component: component,
+      meta: {
+        title,
+      },
     });
   }
-});
-export default router;
+
+  routes.push({
+    path: '/:pathMatch(.*)',
+    redirect: '/notfound',
+  });
+
+  return routes;
+}
+
+async function setupRouter() {
+  const routes = await generateRoutes();
+
+  const router = createRouter({
+    history: createWebHistory(),
+    routes,
+  });
+  router.beforeEach((to: any, from: any, next) => {
+    console.log(to, from, 'to, from', getToken(), whiteList.includes(to.path));
+    if (!getToken() && !whiteList.includes(to.path)) {
+      next(LOGIN_PATH);
+    } else {
+      next();
+
+      document.title = to.meta.title;
+      // if (to.name === routerStack[routerStack.length - 1]) {
+      //   // 这里是后退
+      //   routerStack.pop();
+      //   from.meta.keepAlive = false;
+      //   to.meta.keepAlive = true;
+      // } else {
+      //   // 这里是前进
+      //   routerStack.push(to.name);
+      // }
+      // document.title = to.meta.title;
+      // setTimeout(() => {
+      //   next();
+      // });
+    }
+  });
+  return router;
+}
+
+const routerPromise = setupRouter();
+
+export default routerPromise;
+
+function replace(str: string): string {
+  return str.replace(/[-_]/g, '/');
+}
+
+function division(str: string): string[] {
+  return str.split('/');
+}
+
+function toHump(arr: string[]): string {
+  return arr.reduce(
+    (previous, current) => previous + current.replace(/^\w/, (s) => s.toUpperCase()),
+    '',
+  );
+}
+
+function conversionName(name: string) {
+  return composeRight(toHump, division, replace)(name);
+}
